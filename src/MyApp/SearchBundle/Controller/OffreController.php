df18e9da57c7165d19cd71d66dd37d87c9936ed3
root <root@JEMACOM>
2014-12-12 02:42:19 +0100

    enhancing search feature

--- src/MyApp/SearchBundle/Controller/OffreController.php
+++ src/MyApp/SearchBundle/Controller/OffreController.php
@@ -7,17 +7,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class OffreController extends Controller {
 
     public function searchAction() {
+        $Request = $this->get('request');
         
-        $em = $this->getDoctrine()->getManager();
+        $titre = $Request->get('titre');
+        $type = $Request->get('type');
+        $categorie = $Request->get('categorie');
+        $ville = $Request->get('ville');
+        $prix = $Request->get('prix');
+        $surface = $Request->get('surface'); 
         
-        
-        $Request = $this->getRequest();
-        $search = $Request->get('titre');
-        $offres = $em->getRepository('MyAppProjetBundle:Offre')->findBy(array("titre" => $search));
-        
- 
+          if ($titre!=null && $type!=null && $ville != null && $prix != null && $categorie != null && $surface != null) 
+            {
+            $em=$this->getDoctrine()->getManager();
+            $qb=$em->createQueryBuilder();
+            $qb->select('o');
+            $qb->from('MyAppProjetBundle:Offre','o')->where('o.titre like :titre')
+                    ->setParameter('titre','%'.$titre.'%')
+                    ->andwhere('o.type like :type')
+                    ->setParameter('type' , '%'.$type.'%')
+                    ->andwhere('o.categorie = :categorie')
+                    ->setParameter('categorie',$categorie)
+                    ->andwhere('o.ville  like :ville')
+                    ->setParameter('ville' , '%'.$ville.'%')
+                    ->andwhere('o.prix = :prix')
+                    ->setParameter('prix' , $prix)
+                    ->andwhere('o.surface = :surface')
+                    ->setParameter('surface' , $surface);
+            $offres=$qb->getQuery()->getResult();
+            //var_dump($offres);
+            }
         return $this->render('MyAppSearchBundle:Offre:search.html.twig', array(
-         'offres'=>$offres           
+                    'offres' => $offres
         ));
     }
 
